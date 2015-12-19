b9a692446faefbfb39f8ff5284b5b2a67a8ec62a
Aaron Cremona <dubbacremona@gmail.com>
2015-12-19 13:20:24 -0500

    added get spotify track method

--- app.js
+++ app.js
@@ -1,10 +1,2 @@
-$(document).ready(function(){
-	$.getJSON("https://api.spotify.com/v1/tracks/02BSpb2sKuFgs0Go3LRbTj", function(json){
-		console.log(json.album.name);
-		console.log(json.artists[0].name)
-		console.log(json.name);
-		console.log(json.preview_url)
-	});
-
-	quoteMachine.countChar();
-});
+var currSong = quoteMachine.quotes["1"];
+quoteMachine.getSpotifyTrack(currSong.id);
