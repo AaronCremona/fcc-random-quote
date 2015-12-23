43ee6bc4eb5691470c7fccd05c084b2d048c18f8
unknown <aaron.cremona@GEDULAP-AC.EDUPRIZE.LOCAL>
2015-12-23 12:19:04 -0700

    refactored sizing & row setup for title and album art

--- js/app.js
+++ js/app.js
@@ -1,12 +1,11 @@
-// $(document).ready(function(){
-// 	$.getJSON("https://api.spotify.com/v1/tracks/02BSpb2sKuFgs0Go3LRbTj", function(json){
-// 		console.log(json.album.name);
-// 		console.log(json.artists[0].name)
-// 		console.log(json.name);
-// 		console.log(json.preview_url)
-// 		console.log(json.album.images[0].url)
-// 	});
-// });
+function resizeTitle() {
+	
+}
+
+$(document).ready(function(){
+
+
+});
 
 var qm = quoteMachine;
 qm.initQM();
