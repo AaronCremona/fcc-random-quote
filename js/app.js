572d6ca099500c0524b95aa3d6606790bf4defa1
unknown <aaron.cremona@GEDULAP-AC.EDUPRIZE.LOCAL>
2015-12-23 13:20:06 -0700

    refactored left side again and added resize script

--- js/app.js
+++ js/app.js
@@ -1,10 +1,16 @@
-function resizeTitle() {
-	
+function resizeGreyBackground() {
+	var imgWidth = $(".album-art .img").css("width");
+	console.log(imgWidth);
+	$(".left-col-width").css("width", imgWidth);
 }
 
 $(document).ready(function(){
+	resizeGreyBackground();
 
+});
 
+$(window).resize(function() {
+	resizeGreyBackground();
 });
 
 var qm = quoteMachine;
