cde3385bf0ec8978c4f2dedc9cb230a94952fd09
unknown <aaron.cremona@GEDULAP-AC.EDUPRIZE.LOCAL>
2015-12-23 17:24:52 -0700

    changed to min height on lyric container for responsiveness

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ function resize() {
 	var imgWidth = $(".album-art .img").css("width"),
 		albumContainerHeight = $(".album-container").css("height");
 	$(".left-col-width").css("width", imgWidth);
-	$('.lyric-container').css("height", albumContainerHeight);
+	$('.lyric-container').css("min-height", albumContainerHeight);
 }
 
 $(document).ready(function(){
