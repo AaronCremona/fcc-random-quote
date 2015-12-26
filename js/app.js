16b90ea2790a5f6481262574a70d69dd35b82e29
unknown <aaron.cremona@GEDULAP-AC.EDUPRIZE.LOCAL>
2015-12-26 16:38:16 -0700

    layout updates

--- js/app.js
+++ js/app.js
@@ -1,9 +1,20 @@
 function resize() {
-	var imgWidth = $(".album-art .img").css("width"),
-		albumContainerHeight = $(".album-container").css("height");
-
-	$(".left-col-width").css("width", imgWidth);
-	$('.lyric-container').css("min-height", albumContainerHeight);
+  var imgWidth = parseInt($(".album-art .img").css("width"), 10);
+  if (imgWidth === 0) {
+    setTimeout(resize, 400);
+  }
+  $(".left-col-width").css("width", imgWidth + "px");
+  
+  if (parseInt($(window).width,10) >= 992) {
+    var albumContainerHeight = parseInt($(".album-container").css("height"), 10);
+    $('.lyric-container').css("min-height", albumContainerHeight + "px");
+  }
+  
+  var containerHeight = parseInt($(".main-container").css("height"), 10);
+  var backgroundHeight = parseInt($(".background-container").css("height"), 10);
+  if (containerHeight > backgroundHeight) {
+    $(".background-container, .overlay").css("height", containerHeight + "px");
+  }
 }
 
 $(window).load(function(){
